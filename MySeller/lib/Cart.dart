dd947e6889d0bf4e3baf7bc063e2f49cbe34d468
Chilukoa <abhinay.chilukoorie@gmail.com>
2020-08-02 19:27:39 -0700

    flutter all

---
+++ MySeller/lib/Cart.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class MyCart extends StatefulWidget {
+  @override
+  _MyCartState createState() => _MyCartState();
+}
+
+class _MyCartState extends State<MyCart> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('My shopping cart'),backgroundColor: Colors.red,),
+      body:Container(child: Center(child: Text('Pay here!'),),
+      
+    ));
+  }
+}
